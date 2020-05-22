c4e5bb829b9255bff6be34515d1885804cfb0ef5
a130888599 <1332423943@qq.com>
2020-05-22 10:07:22 +0800

    设置页面样式完成

--- src/pages/set/index.vue
+++ src/pages/set/index.vue
@@ -2,7 +2,13 @@
   <div class="set">
     <!-- 用户信息 -->
     <div class="userInfo">
-      <img class="avatar" :src="user.avatarUrl" alt="">
+      <div class="icon">
+        <div class="share btn" @click="share"></div>
+        <div class="avatar-box btn">
+          <img class="avatar" :src="user.avatarUrl" alt />
+        </div>
+        <div class="edit btn" @click="edit"></div>
+      </div>
       <div class="nickName">{{user.nickName}}</div>
     </div>
     <!-- 若未登录，显示登录按键，若已登录，显示具体设置 -->
@@ -10,20 +16,18 @@
       <div v-if="!isLogged" class="login-button">
         <mp-button type="default" size="large" open-type="getUserInfo" @getuserinfo="handleClick">登陆</mp-button>
       </div>
-      <div v-else class="set-box">
-        
-      </div>
+      <div v-else class="set-box">功能暂定</div>
     </div>
   </div>
 </template>
 
 <script>
 // Use Vuex
-import store from './store'
-import mpButton from 'mpvue-weui/src/button'
+import store from "./store";
+import mpButton from "mpvue-weui/src/button";
 
 export default {
-  name: 'SetPage',
+  name: "SetPage",
   components: {
     mpButton
   },
@@ -32,46 +36,84 @@ export default {
       isLogged: false,
       isLoading: false,
       user: {
-        nickName: '用户名',
-        avatarUrl: '../../static/images/profile.jpg',
+        nickName: "用户名",
+        avatarUrl: "../../static/images/avatar.png"
       }
-    }
+    };
   },
-  computed: {
-  },
+  computed: {},
   methods: {
-    handleClick({target}) {
-      this.isLoading = true
-      const { nickName, avatarUrl } = target.userInfo
-      this.user.nickName = nickName
-      this.user.avatarUrl = avatarUrl
-      store.dispatch('Login', this.user)
-      this.isLoading = false
+    handleClick({ target }) {
+      this.isLoading = true;
+      const { nickName, avatarUrl } = target.userInfo;
+      this.user.nickName = nickName;
+      this.user.avatarUrl = avatarUrl;
+      store.dispatch("Login", this.user);
+      this.isLoading = false;
+    },
+    share() {
+      console.log("点击分享");
+    },
+    edit() {
+      console.log('点击编辑');
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-  .userInfo {
+.userInfo {
+  background-color: #fffbe8;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin: 27px auto;
+  margin-top: 100px;
+  width: 80%;
+  max-width: 500px;
+  height: 13em;
+  border-radius: 20px;
+  .icon {
     display: flex;
-    align-items: center;
-    flex-direction: column;
-    margin: 20px;
-    height: 80vh;
-    .avatar {
-      border-radius: 100%;
-      width: 120px;
-      height: 120px;
+    justify-content: space-around;
+    width: 100%;
+    height: 70px;
+    box-sizing: border-box;
+    
+    .btn {
+      width: 30%;
     }
-    .nickName {
-      margin-top: 10px;
+    .share {
+      background: url(../../../static/images/share.png) no-repeat center;
+      background-size: 1em auto;
+    }
+    .edit {
+      background: url(../../../static/images/share.png) no-repeat center;
+      background-size: 1em auto;
+    }
+    .avatar-box {
+      position: relative;
+      .avatar {
+        position: absolute;
+        left: -13%;
+        top: -100%;
+        border-radius: 100%;
+        width: 120px;
+        height: 120px;
+      }
     }
   }
-  
-  .loginBtn {
-    width: 80%;
-    border-radius: 10px;
-    padding: 10px 20px;
+
+  .nickName {
+    margin-top: 10px;
   }
+}
+
+.loginBtn {
+  margin-top: 50vh;
+  width: 80%;
+  border-radius: 10px;
+  padding: 10px 20px;
+}
 </style>
